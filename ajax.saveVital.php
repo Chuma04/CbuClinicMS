395048517b09195683d474db0ac780fbeaa5b869
davidnsai <nsaidave@gmail.com>
2022-09-24 23:04:55 +0200

    Done

--- ajax.saveVital.php
+++ ajax.saveVital.php
@@ -1,4 +1,7 @@
 	<?php
+	require_once('settings/')
+	include_once("sendsms.php");
+
 	if($_SERVER['REQUEST_METHOD']=='POST'){
 			
 			include("classes/class.vital.php");
@@ -6,6 +9,7 @@
 			$lgn = new VITAL();
 			if($lgn->save($id,$bp,$temp))
 			{
+				//send_sms("Your BP is ".$bp." and your temperature is ".$temp.".",$phone);
 				echo 'Vitals Added';
 			}else
 			{
